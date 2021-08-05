547eace4f4c1790e1075b616b2aaeb92234cff54
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-08-05 18:38:29 +0500

    micro-orm finish setup

--- lireddit-server/dist/index.js
+++ lireddit-server/dist/index.js
@@ -1,2 +1,21 @@
-console.log(1234);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({
+    path: '.env',
+});
+const core_1 = require("@mikro-orm/core");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const main = async () => {
+    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    const isConnected = await orm.isConnected();
+    console.log(`CONNECTED --${isConnected}--`);
+    await orm.getMigrator().up();
+};
+main().catch((e) => {
+    console.error(e);
+});
 //# sourceMappingURL=index.js.map
--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -4,17 +4,21 @@ dotenv.config({
     path: '.env',
 })
 import { MikroORM } from '@mikro-orm/core'
-import { Post } from 'entities/Post'
+import { Post } from './entities/Post'
 import microConfig from './mikro-orm.config'
 
-// const main = async () => {
-//     const orm = await MikroORM.init(microConfig)
-//
-//     const post = orm.em.create(Post, { title: 'my first post' })
-//     await orm.em.persistAndFlush(post)
-//     await orm.em.nativeInsert(Post, { title: 'my first post' })
-// }
-//
-// main().catch((e) => {
-//     console.error(e.message)
-// })
+const main = async () => {
+    const orm = await MikroORM.init(microConfig)
+    console.log(`CONNECTED --${await orm.isConnected()}--`)
+    await orm.getMigrator().up()
+
+    // const post = orm.em.create(Post, { title: 'my first post' })
+    // await orm.em.persistAndFlush(post)
+
+    const posts = await orm.em.find(Post, {})
+    console.log(posts)
+}
+
+main().catch((e) => {
+    console.error(e)
+})
